3046b06b2726a4e7e90504ebcd09a80aa285b79d
ovcr_Darktooth <brandonguti@hotmail.fr>
2025-03-25 21:28:06 +0100

    Controlleur un peu plus d'aplomb

--- code/OvcrPlayerControllerX.cs
+++ code/OvcrPlayerControllerX.cs
@@ -1,11 +1,30 @@
 using Sandbox;
-using Sandbox.Citizen;
-using Overcreep;
-namespace XMovement;
+using System;
+using XMovement;
+namespace Overcreep;
 
 public partial class OvcrPlayerControllerX : PlayerWalkControllerComplex
 {
+	public UInt64 SteamId { get; private set; } = 0;
 	[Property] public Currencies Currencies { get; set; }
 	[Property] public Enchantments Enchantments { get; set; }
 	[Property] public Multiplicators Multiplicators { get; set; } 
+
+	public void SetSteamId(ulong steamId)
+    {
+        // Assure-toi que le Steam ID n'est défini qu'une seule fois
+        if (SteamId == 0)
+        {
+            SteamId = steamId;
+			Log.Info($"Steamid {GameObject.Name} : {SteamId}");
+        }
+    }
+
+	protected override void OnStart()
+	{
+		if ( !IsProxy )
+			SetSteamId(GameObject.Network.Owner.SteamId);	
+			
+		base.OnStart();
+	}
 }
